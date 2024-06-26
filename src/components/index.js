45a2717b18c6e3c9f6bd51d02b5eabcf737487d8
Megagig <megagigsoftwaresolutions.com>
2024-06-26 11:06:27 +0100

    ProductDetails Markup

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,9 @@
-export { Header } from './Layouts/Header';
-export { Footer } from './Layouts/Footer';
+// export { Header } from './Layouts/Header';
+
+// export { Footer } from './Layouts/Footer';
+
+// export { ProductCard } from './Elements/ProductCard';
+
+// // export { Rating } from './Elements/Rating';
+
+// // export { ScrollToTop } from './Others/ScrollToTop';
