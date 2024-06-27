5036484ff457f7de29101a216d9ce3b72a60db16
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-06-27 10:57:10 +0100

    refactor: Adjust products page markup

--- src/pages/Products/ProductsList.jsx
+++ src/pages/Products/ProductsList.jsx
@@ -5,47 +5,78 @@ import { FilterBar } from "./components.js/FilterBar";
 import { ProductCard } from "./components.js/ProductCard";
 
 export const ProductsList = () => {
-  const baseUrl = "http://localhost:3000"
-  const [products, updateProducts] = useState([]);
+    const baseUrl = "https://paschal.pythonanywhere.com/api/v1/";
+    // const baseUrl = "https://fakestoreapi.com";
+    // const baseUrl = "http://localhost:3000"
+    const [products, setProducts] = useState([]);
 
-  useEffect((input) => {
-    axios.get(`${baseUrl}/products`)
-      .then(response => {
-        console.log(response.data);
-        updateProducts(response.data);
-      })
-      .catch(error => console.log(error));
-  }, []);
+    useEffect(() => {
+        axios
+            .get(`${baseUrl}/products`)
+            .then((response) => {
+                console.log(response.data);
+                setProducts(response.data);
+            })
+            .catch((error) => console.log(error));
+    }, []);
 
-  
-  const categoriesChecked = (categoryName = "Category 1") => {
-    // filter products by checked categories
-    const productsList = [...products];
+    const categoriesChecked = (categoryName = "Category 1") => {
+        // filter products by checked categories
+        const productsList = [...products];
 
-    Array.from(productsList).forEach(item => item.category === "Category 1"? item: "");
-    // console.log(productsList);
+        Array.from(productsList).forEach((item) =>
+            item.category === "Category 1" ? item : ""
+        );
+        // console.log(productsList);
 
-    // Solve issue on categories the above code is not filtering.
-  
-  }
+        // Solve issue on categories the above code is not filtering.
+    };
 
-  return (
-  <div className="flex w-full box-border">
-    <FilterBar />
-    <div className="my-6" onClick={() => categoriesChecked("Category 1")}>
-      <p className="ml-4">Showing all {products.length} available products.</p>
-      <div id="productList" className="flex w-full flex-wrap justify-center">
-        {Array.from(products).map(product => 
-          <ProductCard 
-            name = {product.name}
-            price = {product.price}
-            description = {product.description}
-            undiscounted_price = {product.undiscounted_price}
-            src = {product.image}
-            id = {product.id}
-            key = {product.id}
-          />)}
-      </div>
-    </div>
-  </div>);
+    return (
+        <div className="flex flex-col gap-6 p-6 max-w-[1280px] m-auto">
+          <h1 className="text-xl font-bold">Products</h1>
+            <div
+                id="productList"
+                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"
+            >
+                {Array.from(products).map((product) => (
+                    <ProductCard
+                        name={product.name}
+                        price={product.price}
+                        description={product.description}
+                        category={product.category}
+                        src={product.image}
+                        id={product.id}
+                        key={product.id}
+                    />
+                ))}
+            </div>
+        </div>
+        // <div className="flex w-full box-border">
+        //     <FilterBar />
+        //     <div
+        //         className="my-6"
+        //         onClick={() => categoriesChecked("Category 1")}
+        //     >
+        //         <p className="ml-4">
+        //             Showing all {products.length} available products.
+        //         </p>
+        //         <div
+        //             id="productList"
+        //             className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3"
+        //         >
+        //             {Array.from(products).map((product) => (
+        //                 <ProductCard
+        //                     name={product.name}
+        //                     price={product.price}
+        //                     description={product.description}
+        //                     src={product.image}
+        //                     id={product.id}
+        //                     key={product.id}
+        //                 />
+        //             ))}
+        //         </div>
+        //     </div>
+        // </div>
+    );
 };
