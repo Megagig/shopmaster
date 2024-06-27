e1c5f5b67c2c69daba89902ad421beb1eb5392ce
Omoniyi Opemipo [Electron] <omoniyiopempo6@gmail.com>
2024-06-27 18:43:27 +0100

    update: worked on product filtering by category selection

--- src/pages/Products/components.js/FilterBar.jsx
+++ src/pages/Products/components.js/FilterBar.jsx
@@ -1,34 +1,33 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export function FilterBar (){
+export function FilterBar (props){
+  const baseUrl = "https://fakestoreapi.com";
   const [categories, updateCategories] = useState([]);
 
   useEffect(() => {
-    //changed the url by moving the json to the public folder
-    axios.get("https:localhost:3000/products")
-      .then(response => {
-        console.log(response.data);
-        updateCategories(response.data.categories);
-      })
-      .catch(error => console.log(error));
-  }, []);
-  console.log({categories})
+    const fetchData = async () => {
+      try {
+        const response  =  await axios.get(`${baseUrl}/products/categories`);
+        updateCategories(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
-  return <div className={"hidden w-[40vw] md:block"}>
-    <header className="flex justify-between my-3 mt-7 px-4">
-      <h1 className="font-extrabold inline">Product Categories</h1>
-      <svg xmlns="http://www.w3.org/2000/svg" className="size-6 hover:cursor-pointer" fill="none" viewBox="0 0 24 24" 
-      strokeWidth={1.5} stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-      </svg>
+    fetchData();
+  }, []);
+
+  return <div className={"hidden w-[40vw] md:block md:w-[50vw]"}>
+    <header className="flex justify-between my-6 mt-7 px-4">
+      <h1 className="font-extrabold inline">Categories</h1>
     </header>
 
     {categories.map(category => {
       return (
         <div key={category.id} className="flex mx-4 my-3 gap-4">
-          <input type="checkbox" className="lg:w-8" name={category.name} id={category.id} />
-          <h2 className="font-normal text-sm">{category.name}</h2>
+          <input type="checkbox" className="lg:w-8" name={category} id={category} onChange = {(e) => props.checkboxClickedHandler(e)} />
+          <h2 className="font-normal text-sm">{category}</h2>
         </div>
       );
     })}
